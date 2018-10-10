a8d247e662f1e48fded21cbf8eb4d646e80d15ac
BT_1N3_28@bktech.local <BT_1N3_28@bktech.local>
2018-10-10 08:43:46 -0400

    Initial commit

---
+++ src/YanHuang/Main.java
@@ -0,0 +1,20 @@
+package YanHuang;
+
+public class Scanner{
+
+    public static void (String[] args) {
+	int number = Math.random();
+	int guess="";
+	Scanner input = new Scanner(System.in);
+	System.out.print("Can you guess my number?");
+	System guess = input.nextInt();
+
+	While boolean (guess!=number);
+        {
+            System.out.println("Try again");
+        }
+	System.out.println("You got it!");
+    }
+
+
+}
